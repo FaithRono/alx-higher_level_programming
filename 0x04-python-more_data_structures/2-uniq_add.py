37099e9e3e0be3a667beae80a48fd4701d01d1a0
FaithRono <faithrono132@gmail.com>
2023-11-08 09:59:17 -0800

    unique add

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,10 +1,3 @@
+#!/usr/bin/python3
 def uniq_add(my_list=[]):
-    unique_sum = 0  # Initialize a variable to store the sum of unique integers
-    seen = set()   # Create a set to keep track of seen integers
-    
-    for num in my_list:
-        if num not in seen:
-            unique_sum += num   # Add the unique integer to the sum
-            seen.add(num)       # Add the integer to the set to mark it as seen
-    
-    return unique_sum
+    return (sum(set(my_list)))
