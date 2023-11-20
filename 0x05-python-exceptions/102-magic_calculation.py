882affa3ef2bf69715c6922163ad2dd2b97591f3
FaithRono <faithrono132@gmail.com>
2023-11-20 05:25:00 -0800

    magic calculation

---
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def magic_calculation(a, b):
+    result = 0
+    for i in range(1, 4):
+        if i > a:
+            raise Exception('Too far')
+        result = result + a ** b
+        result = result / i
+    result = result + a + b
+    return result
