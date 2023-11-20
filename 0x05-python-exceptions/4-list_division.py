35ae169b2d7b50038c404bb1a09820fa60d4ba09
FaithRono <faithrono132@gmail.com>
2023-11-20 03:03:20 -0800

    list division

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def list_division(my_list_1, my_list_2, list_length):
-    result = []  # Initialize an empty list to store division results
+    result = []
 
     for i in range(list_length):
         try:
@@ -15,6 +15,6 @@ def list_division(my_list_1, my_list_2, list_length):
             print("out of range")
             result.append(0)
         finally:
-            pass  # No specific action needed in the finally block
+            pass
 
     return result
