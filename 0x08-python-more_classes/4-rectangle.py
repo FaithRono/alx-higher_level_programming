09de554e969d7ff2c9495102539ae7eb756583b6
faith rono <132823200+FaithRono@users.noreply.github.com>
2023-11-27 12:16:24 +0000

    class Rectangle that defines a rectangle

--- 0x08-python-more_classes/4-rectangle.py
+++ 0x08-python-more_classes/4-rectangle.py
@@ -108,15 +108,10 @@ class Rectangle:
             [rect.append('#') for j in range(self.__width)]
             if i != self.__height - 1:
                 rect.append("\n")
-            return ("".join(rect))
+        return ("".join(rect))
 
     def __repr__(self):
-        """
-        Return the string representation of the rectangle for reproduction.
-
-        Returns:
-            str: The string representation of the rectangle.
-        """
+        """Return the string representation of the Rectangle."""
         rect = "Rectangle(" + str(self.__width)
         rect += ", " + str(self.__height) + ")"
         return (rect)
