e5cfaad9ae9a8d8e514e76c65c8050a6cde3e713
FaithRono <faithrono132@gmail.com>
2023-12-04 02:01:31 -0800

    inheritance

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,17 @@
+class MyList(list):
+    """
+    A custom list class that inherits from the built-in list class.
+    """
+
+
+     def __init__(self):
+        """initializes the object"""
+        super().__init__()
+
+
+    def print_sorted(self):
+        """
+        Prints the list in sorted (ascending) order.
+        """
+        sorted_list = sorted(self)
+        print(sorted_list)
