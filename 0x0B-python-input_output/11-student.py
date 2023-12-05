1fcf537029540e31093146a9d50614cd0b16f017
FaithRono <faithrono132@gmail.com>
2023-12-05 11:54:51 -0800

    Student to disk and reload

--- 0x0B-python-input_output/11-student.py
+++ 0x0B-python-input_output/11-student.py
@@ -1,65 +1,32 @@
 #!/usr/bin/python3
+""" My Module for creating a Student class
 """
-Module that defines the class Student
-"""
 
 
-class Student:
-    """
-    Class to create student instances
-
-    Attributes:
-        first_name (str): The first name of the student.
-        last_name (str): The last name of the student.
-        age (int): The age of the student.
-    """
+class Student():
+    """ Creates a class Student"""
 
     def __init__(self, first_name, last_name, age):
-        """
-        Special method to initialize a Student instance.
-
-        Args:
-            first_name (str): The first name of the student.
-            last_name (str): The last name of the student.
-            age (int): The age of the student.
-        """
+        """ Initializes a student"""
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self, attrs=None):
-        """
-        Method that returns a dictionary representation of the Student instance.
-
+        """ Returns dictionary description of the instance
         Args:
-            attrs (list): A list of attribute names to be retrieved.
-                If None, all attributes will be retrieved. Default is None.
-
-        Returns:
-            dict: A dictionary representation of the Student instance.
-        """
-        obj = self.__dict__.copy()
-        if isinstance(attrs, list):
-            for item in attrs:
-                if not isinstance(item, str):
-                    return obj
-
-            d_list = {}
-            for iatr in range(len(attrs)):
-                for satr in obj:
-                    if attrs[iatr] == satr:
-                        d_list[satr] = obj[satr]
-            return d_list
-
-        return obj
+            attrs (list): List of strings
+                          to check for in the dict representation"""
+        my_dict = {}
+        new_dict = self.__dict__
+        if attrs is None:
+            return new_dict
+        for key, value in new_dict.items():
+            if key in attrs:
+                my_dict[key] = value
+        return my_dict
 
     def reload_from_json(self, json):
-        """
-        Replaces all attributes of the Student instance with
-        values from a JSON dictionary.
-
-        Args:
-            json (dict): A dictionary containing attribute-value pairs.
-        """
-        for atr in json:
-            self.__dict__[atr] = json[atr]
+        """ Reassembles class attributes from json text"""
+        for key, value in json.items():
+            setattr(self, key, value)
