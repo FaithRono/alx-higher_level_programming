b54eb0b3f9b38366564286197a43fe67e3ac3cdf
FaithRono <faithrono132@gmail.com>
2023-12-05 08:47:55 -0800

    script that reads stdin line by line and computes metrics:

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
+
 import sys
+
 
 def print_metrics(total_size, status_codes):
     """
@@ -7,11 +9,13 @@ def print_metrics(total_size, status_codes):
 
     Args:
         total_size (int): The total file size.
-        status_codes (dict): A dictionary containing the count of each status code.
+        status_codes (dict): A dictionary containing the count
+        of each status code.
     """
     print("File size: {}".format(total_size))
     for code in sorted(status_codes.keys()):
         print("{}: {}".format(code, status_codes[code]))
+
 
 total_size = 0
 status_codes = {}
@@ -20,7 +24,10 @@ try:
     line_count = 0
     for line in sys.stdin:
         try:
-            ip, _, _, status_code, file_size = line.strip().split(' ')[0], line.strip().split(' ')[-2], line.strip().split(' ')[-1]
+            line = line.strip()
+            ip = line.split(' ')[0]
+            _, _, status_code, file_size = line.split(' ')[-3:]
+
             total_size += int(file_size)
             if status_code not in status_codes:
                 status_codes[status_code] = 1
