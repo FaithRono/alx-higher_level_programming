adfcc8835b37ba0c11541191f10a170f76b100ca
FaithRono <faithrono132@gmail.com>
2023-12-05 07:30:44 -0800

    Pascal's Triangle

---
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+
+def pascal_triangle(n):
+    """
+    Generates Pascal's Triangle with n rows.
+
+    Args:
+        n (int): Number of rows in the Pascal's Triangle.
+
+    Returns:
+        list of lists: A list of lists representing Pascal's Triangle.
+
+    If n <= 0, returns an empty list.
+    """
+
+    if n <= 0:
+        return []
+
+    triangle = []
+    for i in range(n):
+        row = [1]
+        if i > 0:
+            prev_row = triangle[i - 1]
+            for j in range(1, i):
+                row.append(prev_row[j - 1] + prev_row[j])
+            row.append(1)
+        triangle.append(row)
+
+    return triangle
