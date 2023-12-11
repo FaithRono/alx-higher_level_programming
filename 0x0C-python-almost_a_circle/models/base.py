1c503cebd9a74002130f58b3f6fd19c8078db118
FaithRono <faithrono132@gmail.com>
2023-12-11 07:21:06 -0800

    corrections

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -43,3 +43,94 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """
+        Returns the JSON string representation of list_dictionaries.
+
+        Args:
+            list_dictionaries (list): List of dictionaries.
+
+        Returns:
+            str: JSON string representation of list_dictionaries.
+        """
+        if list_dictionaries is None or list_dictionaries == []:
+            return "[]"
+        return json.dumps(list_dictionaries)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """
+        Writes the JSON string representation of list_objs to a file.
+
+        Args:
+            cls: The class.
+            list_objs (list): List of instances to be serialized.
+
+        Returns:
+            None
+        """
+        filename = cls.__name__ + ".json"
+        with open(filename, "w") as file:
+            if list_objs:
+                list_dicts = [obj.to_dictionary() for obj in list_objs]
+                json_str = cls.to_json_string(list_dicts)
+                file.write(json_str)
+            else:
+                file.write("[]")
+
+    @staticmethod
+    def from_json_string(json_string):
+        """
+        Returns the list of dictionaries represented by json_string.
+
+        Args:
+            json_string (str): JSON string.
+
+        Returns:
+            list: List of dictionaries represented by json_string.
+        """
+        return json.loads(json_string) if json_string else []
+
+    @classmethod
+    def create(cls, **dictionary):
+        """
+        Returns an instance with attributes set based on dictionary.
+
+        Args:
+            cls: The class.
+            **dictionary: Double pointer to a dictionary
+
+        Returns:
+            obj: An instance with attributes set based on dictionary.
+        """
+        if cls.__name__ == "Rectangle":
+            new_instance = cls(1, 1)
+        elif cls.__name__ == "Square":
+            new_instance = cls(1)
+        else:
+            return None
+
+        new_instance.update(**dictionary)
+        return new_instance
+
+    @classmethod
+    def load_from_file(cls):
+        """
+        Returns a list of instances loaded from a file.
+
+        Args:
+            cls: The class.
+
+        Returns:
+            list: List of instances loaded from a file.
+        """
+        filename = cls.__name__ + ".json"
+        try:
+            with open(filename, "r") as file:
+                json_str = file.read()
+                list_dicts = cls.from_json_string(json_str)
+                return [cls.create(**d) for d in list_dicts]
+        except FileNotFoundError:
+            return []
