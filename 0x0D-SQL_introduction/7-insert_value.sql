acd0188ea51ab97d39d106c004954f086c96f108
faith rono <faithrono132@gmail.com>
2024-01-16 03:42:04 -0800

    inserting rows

--- 0x0D-SQL_introduction/7-insert_value.sql
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -1,4 +1,2 @@
-USE hbtn_0c_0
-
--- Insert the new row
-INSERT INTO first_table(id, name) VALUES(89, 'Best School')
+-- Script to insert three new rows into the table first_table
+INSERT INTO first_table (id, name) VALUES (89, 'Best School');
