562c92a784819ba2abf6d267059fe036a19c5a71
faith rono <faithrono132@gmail.com>
2024-01-16 03:29:16 -0800

    inserting values to a table

--- 0x0D-SQL_introduction/7-insert_value.sql
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -1,17 +1,4 @@
-USE hbtn_0c_0;
+USE hbtn_0c_0
 
--- Create the first_table if it doesn't exist
-CREATE TABLE IF NOT EXISTS first_table (
-  id INT,
-  name VARCHAR(255)
-);
-
--- Check if the new row already exists
-SELECT * FROM first_table WHERE id = 89 AND name = 'Best School';
-
--- Insert the new row if it doesn't exist
-INSERT INTO first_table (id, name)
-SELECT * FROM (SELECT 89, 'Best School') AS tmp
-WHERE NOT EXISTS (
-  SELECT id, name FROM first_table WHERE id = 89 AND name = 'Best School'
-);
+-- Insert the new row
+INSERT INTO first_table(id, name) VALUES(89, 'Best School')
