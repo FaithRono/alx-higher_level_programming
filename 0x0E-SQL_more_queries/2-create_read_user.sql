3cfead9dd226fc847ffc21184d1e4292508ab8ee
faith rono <faithrono132@gmail.com>
2024-01-17 04:04:35 -0800

    script that creates the database hbtn_0d_2 and the user user_0d_2.

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,11 +1,11 @@
--- Create database if it does not exist
+-- Script that Create database if it does not exist
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
 
--- Create user if it does not exist
+-- If user does not exist create it
 CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
 
 -- Grant privileges to the user
 GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
 
--- Flush privileges to apply changes
+-- Use flush to apply the prividlege changes
 FLUSH PRIVILEGES;
