340e615f5c876d0538b24b5c4e0aa0592350fe35
faith rono <faithrono132@gmail.com>
2024-01-17 03:54:15 -0800

    script that creates the MySQL server user user_0d_1

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,4 +1,4 @@
--- Create user user_0d_1 if it does not exist
+-- Script that Create user user_0d_1 if it does not exist
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
 
 -- Grant all privileges to user_0d_1
