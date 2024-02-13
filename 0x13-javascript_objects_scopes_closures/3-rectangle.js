47052fae84d8d68888236500623aa32ec9f4b2a3
faith rono <faithrono132@gmail.com>
2024-02-13 02:14:35 -0800

    class Rectangle that defines a rectangle:

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -1,19 +1,19 @@
 #!/usr/bin/node
-
-class Rectangle {
-  constructor(w, h) {
-    if (w <= 0 || h <= 0) {
-      return {};
+module.exports = class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
     }
-    this.width = w;
-    this.height = h;
   }
 
-  print() {
+  print () {
+    let space = '';
+    for (let i = 0; i < this.width; i++) {
+      space += 'X';
+    }
     for (let i = 0; i < this.height; i++) {
-      console.log('X'.repeat(this.width));
+      console.log(space);
     }
   }
-}
-
-module.exports = Rectangle;
+};
