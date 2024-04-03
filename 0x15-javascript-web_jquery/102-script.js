291b0d66deeffdafa195dc06668b09ed62fd0079
faith rono <faithrono132@gmail.com>
2024-04-03 13:02:53 -0700

    And Press Enter

---
+++ 0x15-javascript-web_jquery/102-script.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+    $('#btn_translate').click(function() {
+        var languageCode = $('#language_code').val();
+        $.ajax({
+            url: 'https://www.fourtonfish.com/hellosalut/hello/',
+            type: 'GET',
+            dataType: 'json',
+            data: { lang: languageCode },
+            success: function(response) {
+                $('#hello').text(response.hello);
+            },
+        });
+    });
+  });
